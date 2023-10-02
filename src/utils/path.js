a4867bc8b0f918f27c6c4a059989e4c7daa93e9d
Byron <byronogis@outlook.com>
2023-10-02 21:40:58 +0800

    fix: get home path

--- src/utils/path.js
+++ src/utils/path.js
@@ -1,13 +1,20 @@
 import { site } from '~/setting'
 
 export function getPath(id, path = '', params) {
+  const query = params ? `?${new URLSearchParams(params).toString()}` : ''
+
   const reg = /^\/+|\/+$/g // remove all splash at the start and end of the string
 
   const siteBase = site.basePath.replace(reg, '')
+
+  if (id === 'home') {
+    return `/${siteBase}${query}`
+  }
+
   const idBase = (site.navigation.find(i => i.id === id)?.path ?? '').replace(reg, '')
   path = path.replace(reg, '')
 
-  return ['', ...[siteBase, idBase, path].filter(Boolean)].join('/') + (params ? `?${new URLSearchParams(params).toString()}` : '')
+  return ['', ...[siteBase, idBase, path].filter(Boolean)].join('/') + query
 }
 
 export function goPath(id, path = '', params) {
