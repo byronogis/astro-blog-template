d5bc9e58acdf241b706abf6c953651bdf24af705
Byron <byronogis@outlook.com>
2023-10-02 19:50:14 +0800

    fix(setting): path respond correctly when base is been set

--- src/setting.js
+++ src/setting.js
@@ -4,11 +4,11 @@ export const site = {
   title: 'Astro Starter',
   description: 'Astro Starter Description',
   navigation: [
-    { title: 'Home', path: '/', icon: 'home' },
-    { title: 'Post', path: '/post/', icon: 'post' },
-    { title: 'Tag', path: '/tag/', icon: 'tag' },
-    { title: 'About', path: '/about/', icon: 'info' },
-  ],
+    { id: 'home', title: 'Home', path: '/', icon: 'home' },
+    { id: 'post', title: 'Post', path: '/post/', icon: 'post' },
+    { id: 'tag', title: 'Tag', path: '/tag/', icon: 'tag' },
+    { id: 'about', title: 'About', path: '/about/', icon: 'info' },
+  ].map(i => ({ ...i, path: import.meta.env.BASE_URL.replace(/\/+$/, '') + i.path })),
   iconMap: {
     home: 'i-ic:baseline-home',
     post: 'i-ic:baseline-article',
