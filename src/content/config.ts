a61954d18b6888e4c38d86df54200834e439bbe5
Byron <byronogis@outlook.com>
2023-10-04 21:56:28 +0800

    !chore(config): content validate adjust
    improve image experience

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,23 @@
 import { defineCollection, z } from 'astro:content'
+import { site } from '~/setting'
 
 const blogCollection = defineCollection({
   type: 'content',
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     description: z.string().optional(),
     image: z.object({
-      url: z.string(),
+      url: z.string().startsWith('/') // use of /public
+        .or(z.string().startsWith('http')) // use of external image
+        .or(image().transform(val => val.src)) // use of relative image and support for astro alias
+        .catch(() => '/placeholder-cover.jpg'),
       alt: z.string(),
-    }).optional(),
-    publishedTime: z.date().optional(),
-    modifiedTime: z.date().optional(),
-    author: z.string().optional(),
+    }).default({ url: '/placeholder-cover.jpg', alt: 'placeholder-cover' }),
+    publishedTime: z.date().default(new Date('1970-01-01')),
+    modifiedTime: z.date().default(new Date('1970-01-01')),
+    author: z.string().default(site.author),
     section: z.string().optional(),
-    tags: z.array(z.string()).optional(),
+    tags: z.array(z.string()).default([]),
   }),
 })
 
