e576c2abc85cddf73e606300df9f62270e1ba966
Byron <byronogis@outlook.com>
2023-10-29 23:47:44 +0800

    chore: remove shortcut

--- uno.config.ts
+++ uno.config.ts
@@ -40,7 +40,6 @@ export default defineConfig({
   ],
   shortcuts: {
     'flex-center': 'flex items-center justify-center',
-    'site-tag': 'px-0.5em py-0.25em border-2px border-dashed border-current rounded-0.5em text-nowrap',
     'site-opacity': 'opacity-60',
   },
   theme: {
