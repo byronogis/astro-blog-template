f028132355ba8a515099c0e4a0b203ed33adda53
Byron <byronogis@outlook.com>
2023-11-04 18:49:18 +0800

    feat: add head in post detail

--- uno.config.ts
+++ uno.config.ts
@@ -45,6 +45,7 @@ export default defineConfig({
   shortcuts: {
     'flex-center': 'flex items-center justify-center',
     'site-opacity': 'opacity-60',
+    'site-title': 'text-3xl font-bold text-center py-4',
     'site-section-title': 'text-xl font-bold text-center py-4',
     'site-list': 'grid grid-cols-1 auto-rows-min gap-4 lg:(grid-cols-2 gap-8)',
   },
