2d0a34142738e0ff11a79c39ee25eb3d5d5b76e2
Byron <byronogis@outlook.com>
2023-11-07 11:31:49 +0800

    chore: fix types error

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+// See https://stackoverflow.com/questions/42066421/property-value-does-not-exist-on-type-eventtarget/42066698#42066698
+type HTMLElementEvent<T extends HTMLElement> = Event & {
+  target: T
+  currentTarget: T
+}
