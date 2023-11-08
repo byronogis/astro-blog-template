e1c8e97c84e4dd59fabd5c137ec627c825497b6d
Byron <byronogis@outlook.com>
2023-11-08 16:24:36 +0800

    fix: prevent scroll
    also set width to 100vw to fix box be squished

--- src/utils/scroll.js
+++ src/utils/scroll.js
@@ -1,16 +1,19 @@
 /**
  * Prevent Page Scrolling When a Modal is Open
  * See https://css-tricks.com/prevent-page-scrolling-when-a-modal-is-open/
+ * Update: also set width to 100vw to fix box be squished
  */
 export function pausePageScroll(el = document.body) {
   const scrollY = window.scrollY
   el.style.position = 'fixed'
   el.style.top = `-${scrollY}px`
+  el.style.width = `100vw`
 }
 
 export function restorePageScroll(el = document.body) {
   const scrollY = el.style.top
   el.style.position = ''
   el.style.top = ''
+  el.style.width = ``
   window.scrollTo(0, Number.parseInt(scrollY || '0') * -1)
 }
