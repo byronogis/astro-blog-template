6ba8780117c9e9f2e42e1f79a0600f2ae5323699
Byron <byronogis@outlook.com>
2023-11-09 17:17:49 +0800

    feat: i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import UnoCSS from 'unocss/astro'
 import pagefind from 'astro-pagefind'
 import robotsTxt from 'astro-robots-txt'
 import sitemap from 'astro-sitemap'
+import astroI18next from 'astro-i18next'
 import { site } from './src/setting'
 
 // https://astro.build/config
@@ -16,6 +17,7 @@ export default defineConfig({
     pagefind(),
     robotsTxt(),
     sitemap(),
+    astroI18next(),
   ],
   server: {
     host: true,
