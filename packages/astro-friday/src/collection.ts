e126840e9af0fcab021c56046a5a15478e651c5c
byronogis <byronogis@outlook.com>
2025-10-02 15:44:14 +0800

    chore!: update collection shema

--- packages/astro-friday/src/collection.ts
+++ packages/astro-friday/src/collection.ts
@@ -4,12 +4,15 @@ import { z } from 'astro/zod'
 export function getSchema(config: ResolvedConfig) {
   return z.object({
     title: z.string(),
+    subtitle: z.string().optional(),
     description: z.string().optional(),
-    publishedTime: z.date().default(new Date('1970-01-01')),
-    modifiedTime: z.date().optional(),
+    created: z.date().default(new Date('1970-01-01')),
+    modified: z.date().optional(),
     author: z.string().default(config.author.name),
     series: z.union([z.string(), z.array(z.string())]).optional().default([]).transform(val => Array.isArray(val) ? val : [val]),
     tags: z.union([z.string(), z.array(z.string())]).optional().default([]).transform(val => Array.isArray(val) ? val : [val]),
+    draft: z.boolean().default(false),
+    lang: z.string().optional().default('en'),
   })
 }
 
